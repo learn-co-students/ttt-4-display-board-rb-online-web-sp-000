49497ebf9df1a0aa11330fb4659372c824d72dce
Paul Rosero <paul_rosero@yahoo.com>
2018-12-04 22:40:59 +0000

    Done.

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -137,11 +137,16 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
-      # Should you want to write your own test for this situation,
-      # read the following code and comments.
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 
-      # Can you copy the syntax of the tests above to write a test for a board
-      # entirely filled with Xs?"
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" X | X | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" X | X | X ")
 
       # Define the board with values that should create the desired output
       # *** Edit the line below ***
@@ -176,7 +181,17 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Os' do
-      # Can you copy the syntax of the tests above to write a test for a board
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" O | O | O ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | O | O ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | O | O ")
+        # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
 
       # Hint: You should be able to copy the code in the previous it example
