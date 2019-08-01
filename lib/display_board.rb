aecb69a042221d5b34bb91426bcacd6ea39489d1
Becky Zorn <bczorn@gmail.com>
2019-08-01 23:35:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+row = "-----------"
+
+def display_board([tl=" ", tm=" ", tr=" ", ml=" ", mm=" ", mr=" ", bl=" ", bm=" ", br=" "])
+puts " #{tl} | #{tm} | #{tr} "
+puts row
+puts " #{ml} | #{mm} | #{mr} "
+puts row
+puts " #{bl} | #{bm} | #{br} "
+end
