9ff4b360b299fe775683fc9bc6787a3260499646
Leslie G <lesliegonzalez@me.com>
2020-06-08 18:37:39 +0000

    Done.

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -196,5 +196,4 @@ describe "#display_board in 'lib/display_board.rb" do
       
       # *** Comment the line below by adding a # at the line start ***
       #expect(true).to be(true)
-      
     end
