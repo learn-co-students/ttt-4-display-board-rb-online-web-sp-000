c52683cc56f37790b66e4812bf35cc47cec596fe
Christerle Nounoute <christerlesn@gmail.com>
2020-08-11 16:11:46 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,30 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(the_board) #what's made in a method, stays in a method
+  puts " #{the_board[0]} | #{the_board[1]} | #{the_board[2]} "
+  puts "-----------"
+  puts " #{the_board[3]} | #{the_board[4]} | #{the_board[5]} "
+  puts "-----------"
+  puts " #{the_board[6]} | #{the_board[7]} | #{the_board[8]} "
+end #Building a machine.
+
+# puts "Welcome to Tic Tac Toe!"
+
+# display_board
+
+board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
+
+puts "Turn 1"
+board = [" "," "," "," ","X"," "," "," "," "]
+display_board(board)
+
+puts "A board with X winning via filling the top row"
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+puts "A random board after 5 turns"
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+display_board(board)
+
+puts "A board filled entirely with O"
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+display_board(board)
