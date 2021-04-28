9031c9a06fb9aac3534b7281246300d13f46e5b6
David Polanco <dayvflight@gmail.com>
2021-04-28 01:32:29 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,18 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+#A_tic_tac_toe_board=
+#  puts "   |   |   "
+#  puts "-----------"
+#  puts "   |   |   "
+#  puts "-----------"
+#  puts "   |   |   "
+
+def display_board (array)
+#puts "#{array[0]}"
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------"
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
+  puts "-----------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
+
+end
