ef36ef594e98d2222821acbe7a6571d4206f030a
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-04-30 01:39:50 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,5 +9,5 @@ def display_board
 end
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, 'X', #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}"
+  puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}"
 end
