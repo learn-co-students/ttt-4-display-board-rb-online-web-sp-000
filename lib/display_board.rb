7d7689e5d63992a670afe742449616e8f20da406
Araceli Ramos <celi2455@gmail.com>
2021-06-01 18:00:02 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,8 @@
-board = [" "," "," "," "," "," "," "," "," "]
-
-output = capture_puts{ display_board(board) } if defined?(display_board)
-rows = output.split("\n")
 
 def display_board(board)
-  puts " #{board[0]}  | #{board[1]}  | #{board[2]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{board[3]}  | #{board[4]}  | #{board[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[6]}  | #{board[7]}  | #{board[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-display_board(board)
